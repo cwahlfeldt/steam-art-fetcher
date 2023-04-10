21bbf9f6b8126e0fd841cd6df528de3068c0601f
waffles <chriswahlfeldt@gmail.com>
2023-04-10 12:07:48 -0500

    yep

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    experimental: {
+        largePageDataBytes: 16 * 10000000,
+    },
 }
 
 module.exports = nextConfig
